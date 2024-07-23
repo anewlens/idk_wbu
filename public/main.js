75b5ba89657c95d94df04c7eccf9e3191afd4a58
Robbie Carroll <rccarroll3@gmail.com>
2024-07-22 22:24:16 -0400

    switch to tom tom api

--- public/main.js
+++ public/main.js
@@ -1,84 +1,105 @@
-import apiCall from './modules/apiCall.js'
+import apiCall from "./modules/apiCall.js";
 
-const filters = document.querySelectorAll('.option')
-const priceFilters = document.querySelectorAll('.dollar')
-const errorMessage = document.querySelector('#errorMessage')
-const activateBtn = document.querySelector('#activateBtn')
+const filters = document.querySelectorAll(".option");
+const priceFilters = document.querySelectorAll(".dollar");
+const errorMessage = document.querySelector("#errorMessage");
+const activateBtn = document.querySelector("#activateBtn");
 
 // LOCATION
-let longitude
-let latitude
+let longitude;
+let latitude;
 
 if (navigator.geolocation) {
-    navigator.geolocation
-        .getCurrentPosition(position => {
-            longitude = position.coords.longitude
-            latitude = position.coords.latitude
-        },
-        err => {
-            errorMessage.classList.add('reveal')
-            console.log(err)
-        })
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      longitude = position.coords.longitude;
+      latitude = position.coords.latitude;
+    },
+    (err) => {
+      errorMessage.classList.add("reveal");
+      console.log(err);
+    }
+  );
 }
 
 // EVENT LISTENERS
-filters.forEach(btn => {
-    btn.addEventListener('click', () => {
-        btn.classList.toggle('active')
-    })
-})
+filters.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+  });
+});
 
 priceFilters.forEach((btn, btnI) => {
-    btn.addEventListener('click', () => {
-        const priceContainer = document.querySelector('#priceContainer')
+  btn.addEventListener("click", () => {
+    const priceContainer = document.querySelector("#priceContainer");
 
-        priceFilters.forEach((e, i) => {
-            if (i < btnI && !e.classList.contains('priceSelected')) {
-                e.classList.toggle('priceSelected')
-            } else if (i === btnI && !e.classList.contains('priceSelected')) {
-                e.classList.toggle('priceSelected')
-            } else if (i > btnI && e.classList.contains('priceSelected')) {
-                e.classList.toggle('priceSelected')
-            }
-        })
-        priceContainer.classList.replace(priceContainer.classList.item(0), `dollars${btnI + 1}`)
-    })
-})
+    priceFilters.forEach((e, i) => {
+      if (i < btnI && !e.classList.contains("priceSelected")) {
+        e.classList.toggle("priceSelected");
+      } else if (i === btnI && !e.classList.contains("priceSelected")) {
+        e.classList.toggle("priceSelected");
+      } else if (i > btnI && e.classList.contains("priceSelected")) {
+        e.classList.toggle("priceSelected");
+      }
+    });
+    priceContainer.classList.replace(
+      priceContainer.classList.item(0),
+      `dollars${btnI + 1}`
+    );
+  });
+});
 
-activateBtn.addEventListener('click', async () => {
-    try {
-        const chosenFilters = [...document.querySelectorAll('.active').values()].map(filter => filter.value)
-        const maxPrice = document.querySelectorAll('.priceSelected').length
-        const priceRange = () => 
-            Array.apply(null, {length: maxPrice})
-            .map((e, i) => i+1)
-            .join(', ')
-        
-        await apiCall(latitude, longitude, chosenFilters, priceRange)
-            //DOM ASSIGNMENTS 
-            .then(async ({resultName, resultUrl}) => {
-                const resultTitle = document.querySelector('#resultTitle')
-                const resultBlock = document.querySelector('#resultBlock')
-                const yelpLink = document.querySelector('#yelpLink')
-                const resultLocation = document.querySelector('#resultLocation')
+activateBtn.addEventListener("click", async () => {
+  try {
+    const chosenFilters = [
+      ...document.querySelectorAll(".active").values(),
+    ].map((filter) => filter.value);
+    const maxPrice = document.querySelectorAll(".priceSelected").length;
+    const priceRange = () =>
+      Array.apply(null, { length: maxPrice })
+        .map((e, i) => i + 1)
+        .join(", ");
 
-                if (!resultName) {
-                    resultTitle.innerHTML = 'Nothing matches that search! :('
-                } else if (!resultBlock.classList.contains('reveal')) {
-                    resultTitle.innerHTML = resultName
-                    resultBlock.classList.toggle('reveal')
-                } else {
-                    await resultBlock.classList.toggle('disappear')
-                    setTimeout(() => {
-                        resultTitle.innerHTML = resultName
-                        resultBlock.classList.toggle('disappear')
-                    }, 700);
-                }
+    await apiCall(latitude, longitude, chosenFilters, priceRange)
+      //DOM ASSIGNMENTS
+      .then(async (result) => {
+        console.log(`result: ${JSON.stringify(result)}`);
+        const resultTitle = document.querySelector("#resultTitle");
+        const resultBlock = document.querySelector("#resultBlock");
+        const yelpLink = document.querySelector("#yelpLink");
+        const resultLocation = document.querySelector("#resultLocation");
 
-                yelpLink.setAttribute('href', resultUrl)
-                resultLocation.setAttribute('href', `https://www.google.com/maps/search/${resultName.replace(' ','+')}/@${latitude},${longitude},14z`)
-            })
-    } catch(err) {
-        console.log('error', err)
-    }
-})
+        if (!result) {
+          resultTitle.innerHTML = "Nothing matches that search! :(";
+        } else if (!resultBlock.classList.contains("reveal")) {
+          resultTitle.innerHTML = result.poi.name;
+          resultBlock.classList.toggle("reveal");
+        } else {
+          await resultBlock.classList.toggle("disappear");
+          setTimeout(() => {
+            resultTitle.innerHTML = result.poi.name;
+            resultBlock.classList.toggle("disappear");
+          }, 700);
+        }
+
+        yelpLink.setAttribute(
+          "href",
+          result.poi.url
+            ? `https://${result.poi.url}`
+            : `https://www.google.com/search?&q=${result.poi.name.replace(
+                " ",
+                "+"
+              )}`
+        );
+        resultLocation.setAttribute(
+          "href",
+          `https://www.google.com/maps/search/${result.poi.name.replace(
+            " ",
+            "+"
+          )}/@${latitude},${longitude},14z`
+        );
+      });
+  } catch (err) {
+    console.log("error", err);
+  }
+});
--- server.js
+++ server.js
@@ -1,60 +1,69 @@
 //EXPRESS BOILERPLATE
-const express = require("express")
-const app = express()
-const path = require("path")
-require("dotenv").config()
+const express = require("express");
+const app = express();
+const path = require("path");
+require("dotenv").config();
 
 //DYNAMIC PORT
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-//YELP FUSION FOR NODE JS
-const yelp = require("yelp-fusion")
-const client = yelp.client(process.env.YELP_API)
+//TOM TOM API
 
+function endpointBuilder(lon, lat, filters) {
+  const categories = [
+    { id: 7315003, name: "tradamerican" },
+    { id: 9379004, name: "beer_and_wine" },
+    { id: 7315012, name: "chinese" },
+    { id: 7315015, name: "hotdogs" },
+    { id: 7315019, name: "greek" },
+    { id: 7315025, name: "italian" },
+    { id: 7315078, name: "icecream" },
+    { id: 7315033, name: "mexican" },
+    { id: 7315036, name: "pizza" },
+    { id: 7315148, name: "sushi" },
+    { id: 7315043, name: "seafood" },
+  ];
+
+  const category = filters
+    .map((val) => {
+      return categories.find((cat) => cat.name === val).id;
+    })
+    .join(",");
+
+  return `https://api.tomtom.com/search/2/nearbySearch/.json?key=${
+    process.env.YELP_API
+  }&lat=${lat}&lon=${lon}&radius=8046&limit=100&categorySet=${
+    filters.length === 0 ? "7315" : category
+  }`;
+}
 //MIDDLEWARE
-app.use(express.static("public"))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
 
 //ROUTES
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname + "/public/index.html"))
-)
+);
 
-app.post("/", (req, res) => {
-  console.log(req.body)
+app.post("/", async (req, res) => {
+  console.log(req.body);
 
-  //YELP API CALL
-  client
-    .search({
-      categories:
-        req.body.filters.length === 0 ? "restaurants" : req.body.filters.join(),
-      longitude: req.body.longitude,
-      latitude: req.body.latitude,
-      radius: 8046,
-      limit: 50,
-      radius: 16000,
-      price: req.body.priceRange,
-    })
-    .then((results) => {
-      //DECLARATIONS FOR RANDOM CHOOSER
-      let max = results.jsonBody.businesses.length
-      let random = Math.floor(Math.random() * max)
+  const { filters, longitude, latitude } = req.body;
 
-      //LOGGING RESULTS
-      console.log(
-        "Full Results: ",
-        results.jsonBody.businesses.map((item) => item.name)
-      )
-
-      //SEND RANDOM BUSINESS
-      res.send({
-        random: results.jsonBody.businesses[random],
-      })
-    })
-    .catch((e) => {
-      console.log(e)
-    })
-})
+  // TOM TOM API CALL
+  const endpoint = endpointBuilder(longitude, latitude, filters);
+  console.log(`endpoint: ${endpoint}`);
+  const apiResponse = await fetch(endpoint);
+  console.log(`status: ${apiResponse.status}`);
+  const json = await apiResponse.json();
+  console.log(
+    `RESULTS: ${JSON.stringify(json.results.map((item) => item.poi.name))}`
+  );
+  let max = json.results.length;
+  let random = Math.floor(Math.random() * max);
+  //SEND RANDOM BUSINESS
+  res.send(json.results[random]);
+});
 
 //START SERVER
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
